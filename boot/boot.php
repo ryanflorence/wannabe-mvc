cd85e61f2e5744a9ef10490dc10a2bbd63cc19f7
Ryan Florence <rpflorence@gmail.com>
2010-08-25 14:20:59 -0600

    Renamed ENVIRONMENT to ENV

--- boot/boot.php
+++ boot/boot.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once('config/environment.php');
-require_once('config/environments/' . ENVIRONMENT . '.php');
+require_once('config/environments/' . ENV . '.php');
 require_once('framework/boot/autoload.php');
 require_once('framework/boot/connect_db.php');
 require_once('framework/boot/helpers.php');
--- boot/connect_db.php
+++ boot/connect_db.php
@@ -2,8 +2,8 @@
 // connect database
 $db_conf = YAML::decode_file('config/database.yml');
 $mysql = mysql_connect(
-	$db_conf[ENVIRONMENT]['host'] . ':' . $db_conf[ENVIRONMENT]['port'],
-	$db_conf[ENVIRONMENT]['user'],
-	$db_conf[ENVIRONMENT]['pass']
+	$db_conf[ENV]['host'] . ':' . $db_conf[ENV]['port'],
+	$db_conf[ENV]['user'],
+	$db_conf[ENV]['pass']
 ) or die('Could not connect: ' . mysql_error());
-mysql_select_db($db_conf[ENVIRONMENT]['database']) or die('Could not select database');
+mysql_select_db($db_conf[ENV]['database']) or die('Could not select database');
